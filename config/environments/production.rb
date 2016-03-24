18ce5b9a3c53c3371ae247242143e4e0c7c6a754
Rick Correia <correia.rick@gmail.com>
2016-03-24 22:18:55 +0000

    Modified production rb config under environments

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,5 +79,6 @@ Rails.application.configure do
   config.log_formatter = ::Logger::Formatter.new
 
   # Do not dump schema after migrations.
-  config.active_record.dump_schema_after_migration = false
+  # Commented out the following line to run on heroku
+  #config.active_record.dump_schema_after_migration = false
 end
