dcfb7b63640c5330fa4098290bb5f1598a80f164
Rick Correia <correia.rick@gmail.com>
2016-03-25 21:07:33 +0000

    fixed Contact mailer email address

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,5 +1,5 @@
 class ContactMailer <ActionMailer::Base
-    default to: correia.rick@gmail.com
+    default to: 'correia.rick@gmail.com'
     
     def contact_email(name, email, body)
         @name = name
